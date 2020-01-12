878b935bde2e7794e9352d419d89ea8aa78a4229
WebFlat <y.vacheslav@gmail.com>
2020-01-12 22:35:39 +0200

    push all first page

--- dist/js/main.js
+++ dist/js/main.js
@@ -10614,9 +10614,22 @@ $(document).ready(function(){
 		items: 1,
 		autoplay: true,
 		nav: true,
-		dots: false,
+		dots: true,
 		autoplayHoverPause: true,
 		animateOut: 'fadeOut',
 		smartSpeed: 1000
 	});
+
+	$('.reviews__cards').owlCarousel({
+		loop: true,
+		items: 1,
+		autoplay: true,
+		dots: true,
+		nav: true,
+		autoplayHoverPause: true,
+		animateOut: 'fadeOut',
+		smartSpeed: 800
+	});
+
+
 });
